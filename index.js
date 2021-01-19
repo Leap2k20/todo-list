cf85e805dcc3f428ddd17221015e353b514ad152
Dulguun Munkhbat <mduk96@gmail.com>
2021-01-19 23:14:19 +0800

    changed

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log('Hello World');
+﻿console.log('Hello World');
 
 // Global vars
 var bNewTask = document.querySelector('header .part2 .new-task');
@@ -7,20 +7,48 @@ var bMenu = document.querySelector('header .part1 .menu-icon');
 var bNewProj = document.querySelector('aside .new-proj');
 var bSubmit = document.querySelector('.add-project button');
 var iProjName = document.querySelector('.add-project input');
-var $projects = document.querySelector('.aside');
+var $nav = document.querySelector('aside');
+var wTask = document.querySelector('.add-task');
+var wProj = document.querySelector('.add-project');
 
 // Function
 function fNewProj() {
-    if (iProjName.innerHTML.length < 1) {
-        alert('Proj name bichne uu');
+    if (iProjName.value === '') {
+        alert('Please fill out this field.');
     } else {
-        var eProj = create
+        wProj.classList.toggle('open');
+
+        var $project = document.createElement('div');
+        $project.className = 'project';
+        var $project_button = document.createElement('button');
+        $project_button.innerHTML = '...';
+        $project.append($project_button);
+        var $project_name = document.createElement('span');
+        $project_name.className = 'text';
+        $project_name.innerHTML = iProjName.value;
+        $project.append($project_name);
+        var $project_triangle = document.createElement('span');
+        $project_triangle.className = 'triangle';
+        $project_triangle.innerHTML = '▶︎';
+        $project.append($project_triangle);
+        var $project_extension = document.createElement('div');
+        $project_extension.className = 'extension';
+        $project.append($project_extension);
+        var $project_extension_button1 = document.createElement('button');
+        $project_extension_button1.innerHTML = 'EDIT';
+        $project_extension.append($project_extension_button1);
+        var $project_extension_button2 = document.createElement('button');
+        $project_extension_button1.innerHTML = 'DELETE';
+        $project_extension.append($project_extension_button2);
+
+        $nav.append($project);
+        iProjName.value = '';
     }
 }
 
 // Button
 bNewTask.onclick = () => {
-    document.querySelector('.add-task').classList.toggle('open');
+    wTask.classList.toggle('open');
     document.querySelector('header .part2 .new-task .plus').classList.toggle('open');
 }
 
@@ -29,12 +57,12 @@ bSort.onclick = () => {
 }
 
 bMenu.onclick = () => {
-    document.querySelector('aside').classList.toggle('open');
+    $nav.classList.toggle('open');
     bMenu.classList.toggle('open');
 }
 
 bNewProj.onclick = () => {
-    document.querySelector('.add-project').classList.toggle('open');
+    wProj.classList.toggle('open');
     bSubmit.onclick = fNewProj;
 }
 
