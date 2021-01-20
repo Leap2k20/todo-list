e80cf09d2da9bdf8edea13af48385ecf68ae5088
Battsetseg <bttstsg9090@gmail.com>
2021-01-20 18:18:31 +0800

    issue 2 done

---
+++ index.js
@@ -0,0 +1,16 @@
+var todoList = document.querySelector('#todo-list>div');
+
+
+function onClickButton(){
+    todoList.classList.remove('collapsed');
+}
+
+var modal = document.getElementById('modal');
+
+// var modal = document.getElementById('modal');
+var btn = document.querySelector("#plus-li");
+
+
+btn.onclick = function() {
+    todoList.classList.remove('');
+  }
