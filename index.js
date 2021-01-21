a743b3d80af72a5396e21059c6cdc3f6606fbf7d
Baataraa <75126279+baatar04@users.noreply.github.com>
2021-01-21 20:41:59 +0800

    1

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 var sorter = document.querySelector('.sort-list');
-var sungah = document.querySelector('.todolist');
-
-var sidenav = document.querySelector('.sidenav');
-
 function onClickSort(){
     sorter.classList.toggle('hide');
 }
 
+var sidenav = document.querySelector('.sidenav');
 function onClickMov(){
     sidenav.classList.toggle('mov');
 }
+
+var onclear = document.querySelector('#button');
+var box = document.querySelector('.whiteBox');
+
+onclear.addEventListener('click',function(){
+    box.classList.add('show');
+});
+
+var modalClose = document.querySelector('.modal-close');
+
+modalClose.addEventListener('click',function(){
+    box.classList.remove('show');
+});
+
 
 //    function onClickcollapsed(){
 //      console.log("hellow World");
