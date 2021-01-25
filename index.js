0ae05c75ff8b2965cbd7cad879dedcbfc7357db2
Qlio <mr.unagaldai@gmail.com>
2021-01-25 15:12:57 +0800

    Implemented addTodo Step 1

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var addTask = document.getElementById('add-task');
 var modal = document.getElementById('modal-container');
+
 addTask.onclick = (e) => {
-    console.log(modal);
     modal.style.display = 'block';
 }
 
-modal.onclick = (e) => {
-    modal.style.display = 'none';
-}
+// modal.onclick = (e) => {
+//     modal.style.display = 'none';
+// }
