ee88fc1c35c586254e83f8922749600c3b1cac71
munkhtulga ganbud <ganbudmunkhtulga88@gmail.com>
2021-01-26 00:29:04 +0800

    onclick done button

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-
-
 var list = document.getElementById('0');
 
-var modal = document.getElementById('mod');
+var modal = document.querySelector('mod');
 var ismodalopen = false;
 
 function onclicknewtask() {
@@ -17,4 +15,33 @@ function onclicknewtask() {
 function clickList () {
     console.log('click');
     list.classList.toggle('title');
+}
+
+
+var $doneButton = document.querySelector('.done');
+var $title = document.querySelector('#title');
+var $description = document.querySelector('#description');
+var $dueDate = document.querySelector('#duedate');
+
+$doneButton.onclick = function() {
+    mod.style.display = 'none';
+
+    var newTodo = {
+        title : $title.value,
+        description : $description.value,
+        dueDate : $dueDate.value,
+        priority : 1,
+    };
+
+    console.log(newTodo);
+    addTodo(newTodo);
+
+};
+
+var todos = [];
+
+function addTodo(newTodo) {
+    console.log('old todos', todos);
+    todos.push(newTodo);
+    console.log('newtodos', todos);
 }
